b6cadaa2a96b8d42cae984fab10b619ac929306d
Mikael Messias <mikaelmessias@outlook.com>
2019-05-02 14:58:15 -0300

    Modify the code to be executed correctly by Python 3
    Signed-off-by: Mikael Messias <mikaelmessias@outlook.com>

--- src/parser/pddl_tree.py
+++ src/parser/pddl_tree.py
@@ -74,7 +74,7 @@ class PDDL_Tree (object):
     def print_tree(self, lvl=0):
         """Print the entire tree to the console."""
 
-        print PDDL_Tree.TAB * lvl + str(self.name)
+        print(PDDL_Tree.TAB * lvl + str(self.name))
 
         for child in self.children:
             child.print_tree(lvl + 1)
